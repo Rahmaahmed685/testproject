0dbf724d2508e0727296ec04b5370b3361b8c0bb
Rahmaahmed685 <rahmaahmed8276@email.com>
2024-03-04 01:28:34 +0200

    first_comiit

---
+++ lib/test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        
+        Text("rahma"),
+        Text("ahmed")
+        
+        
+      ],),
+      
+    );
+  }
+}
